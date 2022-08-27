39893da31f55e91fc1546f652fdd1fbf446c2653
Said Belkaz <saidbelkaz6@gmail.com>
2022-08-27 18:48:48 +0100

    i modified in class Article and Add function for getDetails in database

--- App/models/Article.php
+++ App/models/Article.php
@@ -10,5 +10,14 @@ class Article {
 		$stmt = null;
 	}
     
+	static public function getDetails($data){
+		$id = $data;
+		$stmt = DB::connect()->prepare("SELECT * FROM article WHERE id_article=$id");
+		$stmt->execute();
+		return $stmt->fetch(PDO::FETCH_OBJ);
+		$stmt->close();
+		$stmt = null;
+	}
+
 }
 ?>
