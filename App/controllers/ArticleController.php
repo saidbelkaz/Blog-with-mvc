ea13ca0083d9f14ba12a1770a32e425ddb1ae30a
Said Belkaz <saidbelkaz6@gmail.com>
2022-08-27 19:00:24 +0100

    i modified in class ArticleController and Add 2 function for 1 getAllComment and for 2 addComment in Models

--- App/controllers/ArticleController.php
+++ App/controllers/ArticleController.php
@@ -20,6 +20,38 @@ class ArticleController{
 			return 'error server';
 		}
 	}
+    
+	public function getAllComment(){
+		if ($_SERVER["REQUEST_METHOD"] == "POST"){
+			if(isset($_POST['id'])){
+				$data =$_POST['id'];
+				$Article = Article::getAllComment($data);
+				return $Article;
+			}else{
+				return 'ERORR';
+			}
+		}else{
+			return 'error server';
+		}
+	}
+
+	public function addComment(){
+		if(isset($_POST['submit'])){
+			$data = array(
+				'id' => $_POST['idd'],
+				'name' => $_POST['name'],
+				'content' => $_POST['content'],
+			);
+			$result = Article::addComment($data);
+
+			// if($result === 'ok'){
+			// 	Session::set('success','Employé Ajouté');
+			// 	Redirect::to('home');
+			// }else{
+			// 	echo $result;
+			// }
+		}
+	}
 
 }
 ?>
