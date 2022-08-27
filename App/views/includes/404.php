239f40a5a5de6d9d9a1ff05d54de32d8336f9a8f
Said Belkaz <saidbelkaz6@gmail.com>
2022-08-27 19:21:25 +0100

    I created Html of the pageError 404

---
+++ App/views/includes/404.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="row mt-4">
+		<div class="text-center p-5 rounded bg-secondary">
+			<h2 class="text-white">
+				Page Introuvable
+			</h2>
+			<a href="<?php echo BASE_URL;?>" class="btn btn-outline-secondary text-white">Accueil</a>
+		</div>
+	</div>
+</div>
