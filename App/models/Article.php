d165eab0ff69d9a6c0ce831edcbef2624681973d
Said Belkaz <saidbelkaz6@gmail.com>
2022-08-27 18:58:05 +0100

    i modified in class Article and Add 2 function for 1 getAllComment and for 2 addComment in database

--- App/models/Article.php
+++ App/models/Article.php
@@ -18,6 +18,31 @@ class Article {
 		$stmt->close();
 		$stmt = null;
 	}
+    
+	static public function getAllComment($data){
+		$id = $data;
+		$stmt = DB::connect()->prepare("SELECT * FROM comment WHERE id_article=$id ORDER BY id_article DESC");
+		$stmt->execute();
+		return $stmt->fetchAll();
+		$stmt->close();
+		$stmt = null;
+	}
+
+	static public function addComment($data){
+		$stmt = DB::connect()->prepare("INSERT INTO `comment`(`id_article`, `full_name`, `content`, `date_comment`) 
+			VALUES (:id,:full_name,:content,current_timestamp())");
+		$stmt->bindParam(':id',$data['id']);
+		$stmt->bindParam(':full_name',$data['name']);
+		$stmt->bindParam(':content',$data['content']);
+		if($stmt->execute()){
+			return 'ok';
+		}else{
+			return 'error';
+		}
+
+		$stmt->close();
+		$stmt = null;
+	}
 
 }
 ?>
