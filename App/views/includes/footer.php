efb6fa60ea720403f9714de80cd76eabd9c6c708
Said Belkaz <saidbelkaz6@gmail.com>
2022-08-27 18:26:37 +0100

    I created the footer for all files in views

---
+++ App/views/includes/footer.php
@@ -0,0 +1,34 @@
+
+<footer class="footer-distributed">
+			<div class="footer-left">
+				<img src="<?php echo BASE_URL;?>App/views/images/Blog.jpg" alt="logo">
+				<p class="footer-links">
+					<a href="#" class="link-1">Blog</a>
+					<a href="#">contact us</a>
+					
+				</p>
+				<p class="footer-company-name">Blog © 2022</p>
+			</div>
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p>Blog</p>
+				</div>
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+1.555.555.5555</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:contact@rajacasablanca.ma">contact@Blog.ma</a></p>
+				</div>
+			</div>
+
+		</footer>
+
+	<script src="App/views/js/main.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
